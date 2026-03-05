4e654b4fd1e08517079ac745b2acbdc280d46f1b
John Rey Basilio <john-basilio@users.noreply.github.com>
2026-03-05 11:52:45 +0800

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,40 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use clap::{Parser, Subcommand,};
+
+#[derive(Parser)]
+#[command(name = "dex")]
+#[command(version = "0.1.0")]
+#[command(about = "Decomplexified Execution (DEX) is a tool for managing complex workflows across major Linux distributions under one unified interface.")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Install,
+    Uninstall,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(command) => {
+            match command {
+             Command::Install => {
+                println!("Installing DEX...")
+             }   
+             Command::Uninstall => {
+                println!("Uninstalling DEX...")
+             }
+            }
+        }
+        None => {
+            dex_lib::run()
+        }
+    }
+}
+
---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { invoke } = window.__TAURI__.core;
+
+// let greetInputEl;
+// let greetMsgEl;
+
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   greetInputEl = document.querySelector("#greet-input");
+//   greetMsgEl = document.querySelector("#greet-msg");
+//   document.querySelector("#greet-form").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     greet();
+//   });
+// });
+
+// TAB SWITCHER
+document.querySelectorAll('.tab-btn').forEach(button => {
+      button.addEventListener('click', () => {
+        // Remove active from all buttons & contents
+        document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
+        document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+
+        // Add active to clicked button and matching content
+        button.classList.add('active');
+        const tabId = button.getAttribute('data-tab');
+        document.getElementById(tabId).classList.add('active');
+      });
+    });
